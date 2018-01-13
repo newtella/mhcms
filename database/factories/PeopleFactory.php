592de6ac3b990a160c5e5f06252fad2368da7a25
newtella <fl.mlopezgt@gmail.com>
2018-01-13 10:50:19 -0600

    factory de people
    Creacion de factory de la tabla people

--- database/factories/PeopleFactory.php
+++ database/factories/PeopleFactory.php
@@ -4,6 +4,13 @@ use Faker\Generator as Faker;
 
 $factory->define(App\People::class, function (Faker $faker) {
     return [
-        //
+        
+        'cui'  => $faker->randomNumber(15),
+        'firstname'  => $faker->firstName('male'),
+        'lastname'  => $faker->lastName,
+        'address'  => $faker->address,
+        'imageurl' => $faker->image($width, $height, 'cats'),
+
+
     ];
 });
