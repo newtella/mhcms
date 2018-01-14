a0df6f4b1e5789be54e6a6e539e339e7568f97ce
newtella <fl.mlopezgt@gmail.com>
2018-01-13 18:14:43 -0600

    Se cambio nombre a pantalla de inicio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    MuniHuite
                 </div>
 
                 <div class="links">
